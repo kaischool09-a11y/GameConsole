3bd4d6b3449dbf9ff8c915c2ab3d6612ace2cb2d
Owner <stam1@gmail.com>
2025-11-23 14:02:50 +0200

    Add project files.

---
+++ GameConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using GameConsole.Interfaces;
+
+namespace GameConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IGamePlay> games = new List<IGamePlay>();
+            games.Add(new Games.Pong());
+            games.Add(new Games.FluffyBirdGame());
+            games.Add(new Games.PacManGame());
+
+            foreach (var game in games)
+            {
+                game.Play();
+				Console.Write($" Game:{game.Name}");
+				Console.WriteLine($"Score:{game.Score}");
+            }
+
+		}
+    }
+}
